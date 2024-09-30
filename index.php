a78874c29f6bc5fbd328759ebaaf2aaff61e1caf
badzin23 <160626398+badzin23@users.noreply.github.com>
2024-09-30 11:25:42 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
     if(isset($_POST['update_prof'])){
         $id = $_POST['id'];
-       $nome_prof = $_POST['nome_prof'];
+        $nome_prof = $_POST['nome_prof'];
         $cpf_prof = $_POST['cpf_prof'];
 
         $sql = "UPDATE professores SET nome_prof='$nome_prof',cpf_prof='$cpf_prof' WHERE id = $id";
@@ -41,38 +41,22 @@
 
     }
 
-    if(isset($_GET['delete_prof'])){
-        $id = $_GET['delete_prof'];
+    if(isset($_POST['create_aula'])){
+        $nome = $_POST['nome'];
+        $sala = $_POST['sala'];
+        $hora= $_POST['hora'];
+        $data_aula = $_POST['data_aula'];
 
-        $sql = "DELETE FROM professores WHERE id=$id";
+        $sql = "INSERT INTO aulas ( nome, sala, hora, data_aula) 
+        VALUES ( '$nome', '$sala', '$hora', '$data_aula')";
 
         if($conn -> query($sql) === TRUE){
-            echo "Excluido com sucesso!";
+            echo "Nova aula adicionado com sucesso!";
         }else{
             echo "Erro:". $sql."<br>".$conn->error;
         }
 
     }
-
-  $result = $conn -> query("SELECT * FROM professores");
-
-
-  if(isset($_POST['create_aulas'])){
-    $nome = $_POST['nome'];
-    $sala = $_POST['sala'];
-    $hora= $_POST['hora'];
-    $data_aula = $_POST['data_aula'];
-
-    $sql = "INSERT INTO aulas ( nome, sala, hora, data_aula) 
-    VALUES ( '$nome', '$sala', '$hora', '$data_aula')";
-
-    if($conn -> query($sql) === TRUE){
-        echo "Nova aula adicionado com sucesso!";
-    }else{
-        echo "Erro:". $sql."<br>".$conn->error;
-    }
-
-}
 
 if(isset($_POST['update_aulas'])){
     $id = $_POST['id'];
@@ -85,6 +69,19 @@ if(isset($_POST['update_aulas'])){
 
     if($conn -> query($sql) === TRUE){
         echo "Alteração feita com sucesso!";
+    }else{
+        echo "Erro:". $sql."<br>".$conn->error;
+    }
+
+}
+
+if(isset($_GET['delete_prof'])){
+    $id = $_GET['delete_prof'];
+
+    $sql = "DELETE FROM professores WHERE id=$id";
+
+    if($conn -> query($sql) === TRUE){
+        echo "Excluido com sucesso!";
     }else{
         echo "Erro:". $sql."<br>".$conn->error;
     }
@@ -110,17 +107,18 @@ $sql = "SELECT * FROM professores";
 
 $result = $conn -> query($sql);
 
-if ($result -> num_rows > 0){
+if ($result -> num_rows > 0)
     echo "<table border='1'>
     <tr>
         <th>"
-}
+                
 ?>
 
 <html lang="pt_br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>CRUD AULA</title>
 </head>
 <body>
@@ -144,7 +142,7 @@ if ($result -> num_rows > 0){
     <form method="POST">
         nome <input type="text" name="nome"  required><br><br>
         sala <input type="number" name="sala" required><br><br>
-        hora <input type="number" name="hora" required><br><br>
+        hora <input type="time" name="hora" required><br><br>
         data_aula <input type="date" name="data_aula" required><br><br>
         <input type="submit" name="create_aula" value="Adicionar Aula">
     </form>
@@ -174,7 +172,32 @@ if ($result -> num_rows > 0){
                 <td><?php echo $row['nome_prof']; ?></td>
                 <td><?php echo $row['cpf_prof']; ?></td>
                 <td> 
-                    <a href="index.php?delete=<?php echo $row['id'] ?>">Excluir</a>
+                    <a href="index.php?delete_prof=<?php echo $row['id'] ?>">Excluir</a>
+                </td>
+            </tr>
+        <?php } ?>
+s
+    </table>''
+
+    <h2>Tabela Aulas</h2>
+    <table borde="1">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Sala</th>
+            <th>Hora</th>
+            <th>Data_Aula</th>
+        </tr>
+
+        <?php while($row = $result -> fetch_assoc()){ ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['nome']; ?></td>
+                <td><?php echo $row['sala']; ?></td>
+                <td><?php echo $row['hora']; ?></td>
+                <td><?php echo $row['data_aula']; ?></td>
+                <td> 
+                    <a href="index.php?delete_aulas=<?php echo $row['id'] ?>">Excluir</a>
                 </td>
             </tr>
         <?php } ?>
